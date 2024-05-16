cd01f56dab6e46bc1c1f110d5925119ce0e1b7b9
mohamedaish14 <mhmedeash@gmail.com>
2024-05-16 20:02:32 +0300

    finished

--- app.js
+++ app.js
@@ -20,8 +20,13 @@ const contentRoutes=require('./controllers/content')
 const app=express();
 
 //midellware
-
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 
 
